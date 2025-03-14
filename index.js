19d4970c020f703447e21a3b56d4dea19b2fec8f
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-03-14 16:50:57 +0200

    Test_save

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ let persons = [
   }
 ]
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 // Morgan -logging
 morgan.token('body', (request) => {
   if (request.body) {
@@ -41,6 +51,10 @@ morgan.token('body', (request) => {
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // Routes
 app.get('/', (request, response) => {
@@ -63,35 +77,28 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
-
-  // const id = request.params.id
-  // const person = persons.find(person => person.id === id)
-
-  // if (person) {
-  //   response.json(person)
-  // } else {
-  //   response.status(404).end()
-  // }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
-
-// const generateId = (max) => {
-//   return Math.floor(Math.random() * max)
-// }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  // Name already exists
-  const exists = persons.find(person => person.name === body.name)
-  if (exists) {
-    return response.status(400).json({ 
-      error: `Name ${body.name} already exists.`
-    })
-  }
+  // // Name already exists
+  // const exists = persons.find(person => person.name === body.name)
+  // if (exists) {
+  //   return response.status(400).json({ 
+  //     error: `Name ${body.name} already exists.`
+  //   })
+  // }
 
   // Name or number is missing
   if (!body.name) {
@@ -115,12 +122,35 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
